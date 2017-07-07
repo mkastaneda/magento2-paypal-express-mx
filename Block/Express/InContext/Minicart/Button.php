84ccc628792805ad8e4b2e948292123c1d1aff51
Jesus Cardozo <jesus@qbo.tech>
2017-07-06 19:15:59 -0500

    refactor paypal, button card, checkouut, onapage button

---
+++ Block/Express/InContext/Minicart/Button.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Copyright © 2013-2017 Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace qbo\PayPalMX\Block\Express\InContext\Minicart;
+
+use Magento\Paypal\Block\Express\InContext\Minicart\Button as MinicartButton;
+
+/**
+ * Class Button
+ */
+class Button extends MinicartButton
+{
+                
+    public function getImageUrlButtonMx(){
+        return $this->getViewFileUrl('qbo_PayPalMX::img/buttonPpMx.png');
+    }
+}
